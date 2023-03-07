3b6cd0cf4d21d222fe3a2222b2fb1a05524adbdc
dineshi3 <dinesh.iyyandurai@vakilsearch.com>
2023-03-07 18:30:22 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const port = process.env.PORT || 3000;
+
+const HOST_MAP = {
+  prod: '',
+  dev: `localhost:${port}`,
+  qe: ''
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    HOST: HOST_MAP[process.env.NODE_ENV]
+  }
 }
 
 module.exports = nextConfig
