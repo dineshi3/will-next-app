85060c98eec78163bce0e9860d6d4edcba57907c
Dinesh I <dinesh.iyyandurai@vakilsearch.com>
2023-11-02 18:21:23 +0530

    Magic Sign Map Module

--- src/components/MagicsignPage/GetStartedSteps/index.jsx
+++ src/components/MagicsignPage/GetStartedSteps/index.jsx
@@ -10,8 +10,7 @@ const GetStartedSteps = (props) => {
         <h3>{steps.title}</h3>
         <div className={styles.steps}>
             {steps.steps.map((step, index) => (
-            <div className={styles.step}>
-                {' '}
+            <div className={styles.step} key={step}>
                 <h5>Step {index + 1}:</h5> <p>{step}</p>{' '}
             </div>
             ))}
